99725639abb213b4485ca4488c702e8336e46933
Jake Cyr <cyrjake@gmail.com>
2023-01-04 13:55:25 -0500

    Remove unused functions from InputDevices

--- gpt3_assistant/input_devices.py
+++ gpt3_assistant/input_devices.py
@@ -1,6 +1,4 @@
 import logging
-from typing import Mapping
-
 import pyaudio
 
 from gpt3_assistant.models.input_device import InputDevice
@@ -12,38 +10,6 @@ class InputDevices:
     """
 
     pyaudio = pyaudio.PyAudio()
-
-    @staticmethod
-    def get_input_device_index(input_device_name: str) -> int:
-        """
-        Get the input device to use from the user.
-        :param str input_device_name: the optional device name from the user to look for.
-        :return: the index of the input device to use.
-        """
-        logging.debug(
-            f"InputDevices.get_input_device_index called with: {input_device_name}"
-        )
-
-        input_devices = InputDevices.get_list_of_input_devices()
-
-        if len(input_devices) == 0:
-            raise Exception("No input devices detected")
-
-        chosen_input_device = None
-
-        for input_device in input_devices:
-            if input_device["name"] == input_device_name:
-                chosen_input_device = input_device
-                break
-
-        if chosen_input_device is None:
-            logging.debug(
-                f"Input device '{input_device_name}' not found in list: {input_devices}"
-            )
-            raise Exception(f"Input device with name '{input_device_name}' not found")
-
-        logging.debug(f"Found input device index: {chosen_input_device['index']}")
-        return chosen_input_device["index"]
 
     @staticmethod
     def get_list_of_input_devices() -> list[InputDevice]:
@@ -72,7 +38,3 @@ class InputDevices:
 
         logging.info(f"Found {len(input_devices)} input devices.")
         return input_devices
-
-    @staticmethod
-    def get_device_info_by_index(index: int) -> Mapping[str, int | str | float]:
-        return InputDevices.pyaudio.get_device_info_by_index(index)
